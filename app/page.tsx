3a0b5f1dd48d5a32cfd2a5a3101ad2fb5f40c605
JoseFredes <jose.fredes@mayor.cl>
2024-11-23 14:07:45 -0300

    styles: chage home styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import RecordButton from "./components/Record";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid  items-center justify-items-center pt-48 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-3xl font-bold text-black">Cuentanos tu historia</h1>
+      <h1 className="text-3xl font-bold text-black mb-48">
+        Queremos saber de ti y todas las maravillas que tienes para compartir
+      </h1>
+      <main className="flex items-center">
         <RecordButton />
       </main>
     </div>
