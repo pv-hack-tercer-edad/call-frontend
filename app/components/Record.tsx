7452ec285d5fd72ee2e0e793b281a022c25a682d
JoseFredes <jose.fredes@mayor.cl>
2024-11-23 11:45:30 -0300

    Initial commit

---
+++ app/components/Record.tsx
@@ -0,0 +1,24 @@
+export const RecordButton = () => {
+  return (
+    <button
+      className="flex items-center gap-2 rounded-full border border-solid border-transparent transition-colors bg-foreground text-background hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm h-10 px-4"
+      onClick={() => {
+        console.log("Recording...");
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 1a7 7 0 110 14 7 7 0 010-14zm0 2a5 5 0 100 10 5 5 0 000-10zm0 1a4 4 0 110 8 4 4 0 010-8zm0 1a3 3 0 100 6 3 3 0 000-6zm0 1a2 2 0 110 4 2 2 0 010-4zm0 1a1 1 0 100 2 1 1 0 000-2z"
+          clipRule="evenodd"
+        />
+      </svg>
+      Record
+    </button>
+  );
+};
